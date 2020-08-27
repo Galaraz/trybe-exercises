4ca87df83678883ff3d5f234872b852946a7060b
Galaraz <jvictor.si.2013@gmail.com>
2020-08-27 20:37:13 -0300

    Aprendi a usar o code runer huhugit add .

---
+++ bloco_4/dia_1/atividade_4.js
@@ -0,0 +1,28 @@
+
+//declaraçao das variaveis  
+let A , b , C = 0;
+
+A= 10;
+
+
+
+function inversao(){
+
+    if (A > 0 )  {
+
+        --A
+        console.log(A);
+    }
+
+
+
+    else{
+       
+        ++A
+        console.log(A); 
+    }
+
+        
+
+}
+
---
+++ bloco_4/dia_1/atividade_9.js
@@ -0,0 +1,32 @@
+
+//declaraçao das variaveis  
+let A , b , C = 0;
+
+A= 1;
+B= 2;
+C= 3;
+
+
+let isOdd;
+
+
+function maior(){
+
+    if (A % 2 == 0 || b % 2 == 0 || C % 2 ==0)  {
+
+        isOdd= true;
+        console.log( );
+    }
+
+
+
+
+    else{
+        isOdd= false;
+        console.log(); 
+    }
+
+        
+
+}
+
