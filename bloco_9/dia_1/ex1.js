a99d440fab58196e9763e5f3cf949b0da34f2879
Galaraz <jvictor.si.2013@gmail.com>
2020-10-25 14:00:06 -0300

    primeiro dia pago

--- bloco_9/dia_1/ex1.js
+++ bloco_9/dia_1/ex1.js
@@ -1,3 +1,4 @@
+// primeiro
 const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
   `${name} is ${value} ${measurementUnit} apart from the Sun`
 
@@ -24,6 +25,12 @@ const jupiter = {
     measurementUnit: "kilometers"
   },
 };
+//segundinho
+console.log(planetDistanceFromSun(mars)); // A
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
+
+//terceiro
 
 const getPlanet = () => {
     const mars = {
@@ -37,8 +44,65 @@ const getPlanet = () => {
   };
   
   getPlanet(); // imprime Marte depois de 4 segundos
+//quarto
+  const messageDelay = () => Math.floor(Math.random() * 5000);
 
+  const getMarsTemperature = () => {
+    const maxTemperature = 58;
+    return Math.floor(Math.random() * maxTemperature);
+  };
+  
+  const sendMarsTemperature = () => {
+    const currentTemperature = getMarsTemperature();
+    setTimeout(() => console.log(`Mars temperature is: ${currentTemperature} degree Celsius`), messageDelay());
+  };
+  
+  sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
 
-console.log(planetDistanceFromSun(mars)); // A
-setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
-setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
+  //quinto
+  const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+const sendMarsTemperature = (onSuccess) => {
+    const currentTemperature = getMarsTemperature();
+    setTimeout(() => onSuccess(currentTemperature), messageDelay());
+};
+
+sendMarsTemperature(temperatureInFahrenheit);
+sendMarsTemperature(greet);
+
+// sexto 
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`);
+
+const sendMarsTemperature = (onSuccess, onError) => {
+    const currentTemperature = getMarsTemperature();
+    const messageSuccessfullySent = Math.random() <= 0.6;
+    setTimeout(() => {
+        if(messageSuccessfullySent) onSuccess(currentTemperature)
+        else onError('Robot is busy');
+    }, messageDelay());
+  }
+
+sendMarsTemperature(temperatureInFahrenheit, handleError);
+sendMarsTemperature(greet, handleError);
+
